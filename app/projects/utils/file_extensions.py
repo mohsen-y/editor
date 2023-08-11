5b4bb8000a388ada39cf7bd22b878357144b0da5
mohsen yazdanian <yazdanian.my@gmail.com>
2023-08-11 14:08:04 +0330

    feat(utils): add a list of supported file extensions by the editor to projects utils

---
+++ app/projects/utils/file_extensions.py
@@ -0,0 +1,9 @@
+
+CSS_EXTENSIONS = [".css"]
+PHP_EXTENSIONS = [".php", ".phtml", ".php3", ".php4", ".php5", ".phps"]
+SQL_EXTENSIONS = [".sql", ".ddl", ".dml", ".script", ".qry"]
+XML_EXTENSIONS = [".xml", ".xsl", ".xsd", ".res", ".atom", ".svg", ".plist"]
+HTML_EXTENSIONS = [".htm", ".html", ".dhtml", ".shtml", ".xhtml"]
+CLIKE_EXTENSIONS = [".c", ".cc", ".cxx", ".cpp", ".o", ".h", ".cs", ".java"]
+PYTHON_EXTENSIONS = [".py", ".pyc", ".pyd", ".pyi", ".pyo", ".pyw", ".pyz"]
+JAVASCRIPT_EXTENSIONS = [".js", ".cjs", ".mjs"]
